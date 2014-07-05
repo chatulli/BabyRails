330764a17198779610f82f9fc09ae654a0997c28
chatulli <chatulli@gmx.de>
2014-07-05 12:36:50 +0200

    finish layout and routes

---
+++ example_user.rb
@@ -0,0 +1,12 @@
+class User
+  attr_accessor :name, :email #creates getter and setter
+
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+end
